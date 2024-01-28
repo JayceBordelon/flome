910b3a961d08f4489fd0cfa8d7a00db956ae2103
Jayce Bordelon <jaycebordelon@Jayces-MBP.lan>
2024-01-28 16:54:53 -0600

    Blurp

--- src/data/members.jsx
+++ src/data/members.jsx
@@ -1,6 +1,75 @@
 const BASE_IMAGE_PATH = "/members/";
 
 export const MEMBERS = [
+    // Executive Members
+    {
+        "fullName": "Alyssa Lee",
+        "major1": "Biomedical Engineering",
+        "major2": "",
+        "minor1": "Design",
+        "minor2": "",
+        "purpose": "I founded FLOME to establish deeper connections within the McKelvey and St. Louis community and challenge myself as well as others to excel pre-professionally.",
+        "role": "President & Founder",
+        "imagePath": `${BASE_IMAGE_PATH}Alyssa.jpg`,
+        "linkedIn": "https://www.linkedin.com/in/alyssa-lee-504929215/"
+    },
+    {
+        "fullName": "Jayce Bordelon",
+        "major1": "Computer Science",
+        "major2": "",
+        "minor1": "Human Computer Interaction",
+        "minor2": "",
+        "purpose": "I joined FLOME to surround myself with driven engineers as I grow into my professional career. Additionally, FLOME offers ample opportunities for community service and allows me to give back in a way I never could before.",
+        "role": "Vice President",
+        "imagePath": `${BASE_IMAGE_PATH}Jayce.jpg`,
+        "linkedIn": "https://www.linkedin.com/in/jaycebordelon/"
+    },
+    {
+        "fullName": "Tarek Elbialy",
+        "major1": "Computer Science",
+        "major2": "Sociology",
+        "minor1": "",
+        "minor2": "",
+        "purpose": "Joining FLOME has given me the privilege of being in a supportive and diverse community who are passionate about becoming the best young professionals possible and having fun while at it.",
+        "role": "Corresponding Secretary",
+        "imagePath": `${BASE_IMAGE_PATH}Tarek.jpg`,
+        "linkedIn": "https://www.linkedin.com/in/tarek-elbialy-384591224"
+    },
+    {
+        "fullName": "Corin Riahi",
+        "major1": "Mechanical Engineering",
+        "major2": "",
+        "minor1": "Energy Engineering",
+        "minor2": "Drama",
+        "purpose": "I joined FLOME because of the numerous opportunities with professional development as well as the amazing and collaborative community.",
+        "role": "Marketing Director",
+        "imagePath": `${BASE_IMAGE_PATH}Corin.jpg`,
+        "linkedIn": "https://www.linkedin.com/in/corin-riahi/"
+    },
+    {
+        "fullName": "Eman Teshome",
+        "major1": "Computer Science",
+        "major2": "Sociology",
+        "minor1": "",
+        "minor2": "",
+        "purpose": "I joined FLOME because it offers McKelvey students a unique opportunity to participate in a multifaceted club. I appreciate being part of a group that emphasizes not only professional development but also service and community.",
+        "role": "Outreach Chair",
+        "imagePath": `${BASE_IMAGE_PATH}Eman.jpg`,
+        "linkedIn": "https://www.linkedin.com/in/eman-teshome-812824292/"
+    },
+    {
+        "fullName": "Okorie Agwu",
+        "major1": "Mechanical Engineering",
+        "major2": "",
+        "minor1": "",
+        "minor2": "",
+        "purpose": "I believe that one of the many benefits of being a part of a community/club is some sense of belonging. As well as people who are able and willing to help you along your journey, which is very similar to theirs. As a prospective member, FLOME was presented to me as such community and I decided to join.",
+        "role": "Second Outreach Chair",
+        "imagePath": `${BASE_IMAGE_PATH}Okorie.jpg`,
+        "linkedIn": "https://www.linkedin.com/in/okorie-agwu-008b04243/"
+    },
+
+    // Members
     {
         "fullName": "Jonah Sachs",
         "major1": "Computer Science",
@@ -20,7 +89,7 @@ export const MEMBERS = [
         "minor2": "",
         "purpose": "I was really drawn to the idea of a community of people who were interested in the same things as me and were equally motivated.",
         "role": "Member",
-        "imagePath": `${BASE_IMAGE_PATH}Katherine.jpeg`, // Replace with the actual image name
+        "imagePath": `${BASE_IMAGE_PATH}Katherine.jpeg`,
         "linkedIn": "https://www.linkedin.com/in/katherine-feng1/"
     },
     {
@@ -31,7 +100,7 @@ export const MEMBERS = [
         "minor2": "",
         "purpose": "I joined FLOME to form personal connections and develop teamwork skills with fellow engineers in a non-competitive environment. I also want to make sure that I give back to the St. Louis community by keeping volunteering in my schedule through the commitment I made to FLOME. FLOME also has great career-development guidance that I wanted to take advantage of.",
         "role": "Member",
-        "imagePath": `${BASE_IMAGE_PATH}Eliana.png`, // Replace with the actual image name
+        "imagePath": `${BASE_IMAGE_PATH}Eliana.png`,
         "linkedIn": "https://www.linkedin.com/in/eliana-sobel-4b1801289/"
     },
     {
@@ -53,7 +122,7 @@ export const MEMBERS = [
         "minor2": "",
         "purpose": "I joined FLOME to meet and collaborate with other engineering students. Additionally, I saw it as a chance to help further myself professionally.",
         "role": "Member",
-        "imagePath": `${BASE_IMAGE_PATH}Tyler.jpg`, // Replace with the actual image name
+        "imagePath": `${BASE_IMAGE_PATH}Tyler.jpg`,
         "linkedIn": "https://www.linkedin.com/in/tyler-duchscherer-230a8b299/"
     },
     {
@@ -64,7 +133,7 @@ export const MEMBERS = [
         "minor2": "",
         "purpose": "I joined FLOME to connect with fellow engineers on campus and make a difference in the community.",
         "role": "Member",
-        "imagePath": `${BASE_IMAGE_PATH}Hunter.jpg`, // Replace with the actual image name
+        "imagePath": `${BASE_IMAGE_PATH}Hunter.jpg`,
         "linkedIn": "https://www.linkedin.com/in/hunter-rocker-30a484262/"
     },
     {
@@ -75,7 +144,7 @@ export const MEMBERS = [
         "minor2": "",
         "purpose": "I joined FLOME to meet hard-working engineers looking to succeed in school and their careers. I also joined to meet like-minded people who have interesting hobbies and passions.",
         "role": "Member",
-        "imagePath": `${BASE_IMAGE_PATH}Ryan.png`, 
+        "imagePath": `${BASE_IMAGE_PATH}Ryan.png`,
         "linkedIn": "https://www.linkedin.com/in/ryan-schmadtke-115a46270/"
     },
     {
@@ -86,8 +155,41 @@ export const MEMBERS = [
         "minor2": "",
         "purpose": "FLOME offered a community of like-minded students working together towards professional development and career growth.",
         "role": "Member",
-        "imagePath": `${BASE_IMAGE_PATH}Eric.jpg`, // Replace with the actual image name
+        "imagePath": `${BASE_IMAGE_PATH}Eric.jpg`,
         "linkedIn": "https://www.linkedin.com/in/gnawcire/"
+    },
+    {
+        "fullName": "Alex Whalen",
+        "major1": "Biomedical Engineering",
+        "major2": "Biomedical Data Science",
+        "minor1": "",
+        "minor2": "",
+        "purpose": "I joined FLOME because I wanted to have a community of engineers working together. Being able to collaborate and come together as different engineers to achieve the same common goal. I believe that this is important to have connections and help one another out.",
+        "role": "Member",
+        "imagePath": `${BASE_IMAGE_PATH}Alex.jpg`,
+        "linkedIn": "https://www.linkedin.com/in/alexander-whalen-910794258/"
+    },
+    {
+        "fullName": "Joshua Kim",
+        "major1": "Computer Science",
+        "major2": "HCI",
+        "minor1": "Design",
+        "minor2": "",
+        "purpose": "I became a member of FLOME with the aim of connecting with fellow engineering enthusiasts committed to learning and fostering a cooperative environment.",
+        "role": "Member",
+        "imagePath": `${BASE_IMAGE_PATH}Joshua.jpg`,
+        "linkedIn": "https://www.linkedin.com/in/joshua-kim-383a74242"
+    },
+    {
+        "fullName": "Daniel Oh",
+        "major1": "Biomedical Engineering",
+        "major2": "",
+        "minor1": "",
+        "minor2": "",
+        "purpose": "I wanted to meet and get to know other engineering majors around me. I also wanted to make connections with these people to open the possibility of collaboration with them in the future.",
+        "role": "Member",
+        "imagePath": `${BASE_IMAGE_PATH}Daniel.png`,
+        "linkedIn": "https://www.linkedin.com/in/daniel-oh-18b72b251/"
     },
     {
         "fullName": "Dorian Marr",
@@ -110,60 +212,7 @@ export const MEMBERS = [
         "role": "Member",
         "imagePath": `${BASE_IMAGE_PATH}Ben.png`,
         "linkedIn": "https://www.linkedin.com/in/benjamin--mueller"
-    },
-    {
-        "fullName": "Alyssa Lee",
-        "major1": "Biomedical Engineering",
-        "major2": "",
-        "minor1": "Design",
-        "minor2": "",
-        "purpose": "I founded FLOME to establish deeper connections within the McKelvey and St. Louis community and challenge myself as well as others to excel pre-professionally.",
-        "role": "President & Founder",
-        "imagePath": `${BASE_IMAGE_PATH}Alyssa.jpg`, // Replace with the actual image name
-        "linkedIn": "https://www.linkedin.com/in/alyssa-lee-504929215/"
-    },
-    {
-        "fullName": "Jayce Bordelon",
-        "major1": "Computer Science",
-        "major2": "",
-        "minor1": "Human Computer Interaction",
-        "minor2": "",
-        "purpose": "I joined FLOME to surround myself with driven engineers as I grow into my professional career. Additionally, FLOME offers ample opportunities for community service and allows me to give back in a way I never could before.",
-        "role": "Vice President",
-        "imagePath": `${BASE_IMAGE_PATH}Jayce.jpg`,
-        "linkedIn": "https://www.linkedin.com/in/jaycebordelon/"
-    },
-    {
-        "fullName": "Corin Riahi",
-        "major1": "Mechanical Engineering",
-        "major2": "",
-        "minor1": "Energy Engineering",
-        "minor2": "Drama",
-        "purpose": "I joined FLOME because of the numerous opportunities with professional development as well as the amazing and collaborative community.",
-        "role": "Marketing Director",
-        "imagePath": `${BASE_IMAGE_PATH}Corin.jpg`, 
-        "linkedIn": "https://www.linkedin.com/in/corin-riahi/"
-    },
-    {
-        "fullName": "Eman Teshome",
-        "major1": "Computer Science",
-        "major2": "Sociology",
-        "minor1": "",
-        "minor2": "",
-        "purpose": "I joined FLOME because it offers McKelvey students a unique opportunity to participate in a multifaceted club. I appreciate being part of a group that emphasizes not only professional development but also service and community.",
-        "role": "Outreach Chair",
-        "imagePath": `${BASE_IMAGE_PATH}Eman.jpg`, // Replace with the actual image name
-        "linkedIn": "https://www.linkedin.com/in/eman-teshome-812824292/"
-    },
-    {
-        "fullName": "Okorie Agwu",
-        "major1": "Mechanical Engineering",
-        "major2": "",
-        "minor1": "",
-        "minor2": "",
-        "purpose": "I believe that one of the many benefits of being a part of a community/club is some sense of belonging. As well as people who are able and willing to help you along your journey, which is very similar to theirs. As a prospective member, FLOME was presented to me as such community and I decided to join.",
-        "role": "Second Outreach Chair",
-        "imagePath": `${BASE_IMAGE_PATH}Okorie.jpg`, 
-        "linkedIn": "https://www.linkedin.com/in/okorie-agwu-008b04243/"
-    },
+    }
+
+    
 ]
